88757f4aae0a016e3267073ea18c50c40f81393c
UMESHRAUT <rautumesh34@gmail.com>
2020-06-21 07:42:26 +0530

    search

--- src/components/searchNote.js
+++ src/components/searchNote.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 function Search(props){
-
+    const [search,setSearch]=useState('');
+     
+    function Reset(){
+        setSearch('');
+    }
     function handleChange(event){
-
+        setSearch(event.target.value)
         props.returnSearch(event.target.value);
     }
 
-    return <input name='search' placeholder='search for note' className="search" onChange={handleChange} />
+    return<div className="search"><input name='search' placeholder='search for note' className="search-contain" value={search} onChange={handleChange} />
+        <button onClick={Reset} className="search-button">clear</button>
+    </div> 
 }
 export default Search;
